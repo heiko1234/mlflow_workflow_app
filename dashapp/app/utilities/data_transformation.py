2a7e307248a75d0bcec487780cbfcbcea7d09e64
Heiko Kulinna <Heikokulinna@gmx.de>
2023-09-19 20:41:23 +0200

    [HK] data transformation

--- dashapp/app/utilities/data_transformation.py
+++ dashapp/app/utilities/data_transformation.py
@@ -23,4 +23,25 @@ def transform_datapoint(datapoint, transformation=None):
     return data
 
 
+def transform_column(df, column, transformation):
+
+    if transformation == "no transformation":
+        data = df[column]
+    elif transformation == "log":
+        data = df[column].apply(lambda x: np.log(x))
+    elif transformation == "sqrt":
+        data = df[column].apply(lambda x: np.sqrt(x))
+    elif transformation == "1/x":
+        data = df[column].apply(lambda x: 1/x)
+    elif transformation == "x^2":
+        data = df[column].apply(lambda x: x**2)
+    elif transformation == "x^3":
+        data = df[column].apply(lambda x: x**3)
+    else:
+        data = df[column]
+
+    return data
+
+
+
 
